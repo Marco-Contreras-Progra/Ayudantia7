1c748af0e2cf680f471a8d14c117a55984a972eb
Marco-Contreras-Progra <m.contreras28@ufromail.cl>
2024-11-12 19:01:57 -0300

    avance Testing

---
+++ demo/src/test/java/AutomotoraTest.java
@@ -0,0 +1,16 @@
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.example.Principal.Automotora;
+import com.example.Principal.Vehiculo;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+public class AutomotoraTest {
+    private Automotora automotora;
+    private Vehiculo vehiculo1;
+    private Vehiculo vehiculo2;
+}
