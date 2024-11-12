bd61192095391e5e7d1c9d0cc74ea98b0a05ba93
Marco-Contreras-Progra <m.contreras28@ufromail.cl>
2024-11-12 16:11:19 -0300

    cambios en automotora

--- demo/src/main/java/com/example/Principal/Automotora.java
+++ demo/src/main/java/com/example/Principal/Automotora.java
@@ -1,28 +1,53 @@
-package Principal;
+package com.example.Principal;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import Datos.GestorDatos;
 
 public class Automotora {
-	private Vehiculo _vehiculos;
+    private List<Vehiculo> vehiculos;
 	private GestorDatos _gestorDatos;
 
-	public void agregarVehiculo() {
-		throw new UnsupportedOperationException();
-	}
+	    public Automotora() {
+        this.vehiculos = new ArrayList<>();
+    }
+
+	public void agregarVehiculo(Vehiculo vehiculo) {
+        vehiculos.add(vehiculo);
+        System.out.println("Vehículo agregado: " + vehiculo.getMarca() + " " + vehiculo.getModelo());
+    }
 
 	public Vehiculo listarVehiculos() {
-		throw new UnsupportedOperationException();
+		if (vehiculos.isEmpty()) {
+			System.out.println("No hay vehiculos registrados");
+		} else {
+			for (Vehiculo vehiculo : vehiculos){
+                System.out.println("Marca: " + vehiculo.getMarca() + " Modelo:" + vehiculo.getModelo());
+			}
+		}
+	
 	}
 
 	public void motrarDetallesVehiculos() {
-		throw new UnsupportedOperationException();
+		if (vehiculos.isEmpty()) {
+			System.out.println("No hay vehiculos registrados");
+		} else {
+			for (Vehiculo vehiculo : vehiculos){
+			vehiculo.mostrarDetalles();
+			}
+		}
+	
 	}
 
-	public void cargarVehiculosDesdeJSON() {
-		throw new UnsupportedOperationException();
+	public void cargarVehiculosDesdeJSON(String archivo) {
+		GestorDeDatos gestor = new GestorDeDatos();
+        List<Vehiculo> vehiculosCargados = gestor.leerVehiculosDesdeJSON(archivo);
+        vehiculos.addAll(vehiculosCargados);
 	}
 
-	public void guardarVehiculosEnJSON() {
-		throw new UnsupportedOperationException();
+	public void guardarVehiculosEnJSON(String archivo) {
+		GestorDeDatos gestor = new GestorDeDatos();
+        gestor.guardarVehiculosEnJSON(vehiculos, archivo);
 	}
 }
