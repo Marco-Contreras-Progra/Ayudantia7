cecce5b779ea52b339843eb7579e2a0c22eeb661
Marco-Contreras-Progra <m.contreras28@ufromail.cl>
2024-11-12 15:28:20 -0300

    clase auto

--- demo/src/main/java/com/example/Principal/Auto.java
+++ demo/src/main/java/com/example/Principal/Auto.java
@@ -1,5 +1,23 @@
-package Principal;
+package com.example.Principal;
 
 public class Auto extends Vehiculo {
-	private int _numeroDePuertas;
+	private int numeroDePuertas;
+
+	public Auto(String marca, String modelo, int año, double precio, String tipoBicicleta) {
+        super(marca, modelo, año, precio);
+        this.numeroDePuertas = numeroDePuertas;
+	}
+
+	public int getNumeroPuertas(){
+		return numeroDePuertas;
+	}
+	public void setNumeroPuertas(int numeroDePuertas){
+		this.numeroDePuertas = numeroDePuertas;
+	}
+	@Override
+    public void mostrarDetalles() {
+        super.mostrarDetalles();
+        System.out.println("Numero de puertas: " + numeroDePuertas);
+	}
+	
 }
--- demo/src/main/java/com/example/Principal/Camión.java
+++ demo/src/main/java/com/example/Principal/Camión.java
@@ -1,5 +1,24 @@
-package Principal;
+package com.example.Principal;
 
-public class Cami�n extends Vehiculo {
-	private String _capacidadDeCarga;
+public class Camión extends Vehiculo {
+	private String capacidadDeCarga;
+
+	public Camión(String marca, String modelo, int año, double precio, String tipoBicicleta) {
+        super(marca, modelo, año, precio);
+        this.capacidadDeCarga = capacidadDeCarga;
+	}
+
+	public String getCapacidad(){
+		return capacidadDeCarga;
+	}
+	public void setCapacidad(String capacidadDeCarga){
+		this.capacidadDeCarga = capacidadDeCarga;
+	}
+	@Override
+    public void mostrarDetalles() {
+        super.mostrarDetalles();
+        System.out.println("Capacidad de carga:" + capacidadDeCarga);
+	}
+	
+
 }
